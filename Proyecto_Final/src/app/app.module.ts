330d625817874568f961a2ac813c58c914c24282
NoeliaLuduena <noelia.luduena@mi.unc.edu.ar>
2023-02-21 14:37:59 -0300

    acercaDe

--- Proyecto_Final/src/app/app.module.ts
+++ Proyecto_Final/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,

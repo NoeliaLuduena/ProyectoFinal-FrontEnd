ca7c76804f2e1451f37fa324e621c3fb7205876d
NoeliaLuduena <noelia.luduena@mi.unc.edu.ar>
2023-03-11 17:34:38 -0300

    Creación de la carpeta Backend
    Dentro de la carpeta se encuentra el proyecto de Spring

---
+++ Proyecto_Final/Backend/src/test/java/com/portfolio/nbl/NblApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.nbl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NblApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

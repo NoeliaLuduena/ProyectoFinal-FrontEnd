9c9e97480953d9a9e76cd4764062920dedf4d8bf
NoeliaLuduena <noelia.luduena@mi.unc.edu.ar>
2023-03-12 17:58:22 -0300

    Avance en el Backend
    Creación de Entidad, Repositorio, Interface, Servicio, Controlador

---
+++ Proyecto_Final/Backend/src/main/java/com/portfolio/nbl/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.nbl.Repository;
+
+import com.portfolio.nbl.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+    
+}
